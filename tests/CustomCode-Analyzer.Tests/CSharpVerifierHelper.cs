32ae513fe07d06a3d1e21ac580fe564993675dc2
Ruben Jimenez <ruben.jimenez@outsystems.com>
2024-11-13 15:07:33 +0100

    A bit of refactoring on test project

---
+++ tests/CustomCode-Analyzer.Tests/CSharpVerifierHelper.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.CodeAnalysis;
+using System.Collections.Immutable;
+
+namespace CustomCode_Analyzer.Tests
+{
+    // Helper class to configure nullable reference type warnings
+    public static class CSharpVerifierHelper
+    {
+        // Dictionary of nullable warning configurations
+        public static ImmutableDictionary<string, ReportDiagnostic> NullableWarnings
+        {
+            get
+            {
+                return ImmutableDictionary.CreateRange(new[]
+                {
+                        // Configure specific nullable warning codes as errors
+                        new KeyValuePair<string, ReportDiagnostic>("CS8632", ReportDiagnostic.Error),
+                        new KeyValuePair<string, ReportDiagnostic>("CS8669", ReportDiagnostic.Error)
+                    });
+            }
+        }
+    }
+}
